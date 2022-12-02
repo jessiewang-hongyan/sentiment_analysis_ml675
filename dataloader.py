ead801c2c2cfa8bd5ade3a9f658f5b8b93c27d16
jessiewang-hongyan <jessiewang529@gmail.com>
2022-12-02 14:58:48 -0500

    Add bert tokenization

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,35 @@
+import torch
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader, random_split
 
-def get_dataloaders():
-    # TODO: change the dataset to our custom one
-    train_data = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
-    train, val = random_split(train_data, [55000, 5000]) # training size 55000, val size 5000
+class TwitterDataset(Dataset):
+    def __init__(self, attrs, labels):
+        super().__init__()
+        self.attrs = attrs
+        self.labels = labels
 
-    train_loader = DataLoader(train, batch_size=32)
-    val_loader = DataLoader(val, batch_size=32)
+    def __len__(self):
+        return len(self.labels)
 
-    return train_loader, val_loader
+    def __getitem__(self, idx):
+        X = self.attrs[idx, :]
+        y = self.labels[idx]
+        return X, y
+
+def get_dataloaders(X, y):
+    # # TODO: change the dataset to our custom one
+    # train_data = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
+    # train, val = random_split(train_data, [55000, 5000]) # training size 55000, val size 5000
+
+    # train_loader = DataLoader(train, batch_size=32)
+    # val_loader = DataLoader(val, batch_size=32)
+
+    twitter_dataset = TwitterDataset(X, y)
+
+    train_set, val_set, test_set = random_split(twitter_dataset, [0.8, 0.1, 0.1])    
+
+    train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=32, shuffle=True)
+
+    return train_loader, val_loader, test_loader
---
+++ tokenizer.py
@@ -0,0 +1,32 @@
+import torch
+from transformers import BertTokenizer
+
+def tokenize(sentences, labels, max_length = 64):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+    max_len = 0
+    for sent in sentences:
+        input_ids = tokenizer.encode(sent, add_special_tokens=True)
+        max_len = max(max_len, len(input_ids))
+
+    input_ids = []
+    attention_masks = []
+    encoded_dict = tokenizer.encode_plus(
+        sent,  
+        add_special_tokens = True,
+        return_attention_mask = True,  
+        return_tensors = 'pt', 
+        )
+
+    input_ids.append(encoded_dict['input_ids'])
+    attention_masks.append(encoded_dict['attention_mask'])
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    labels = torch.tensor(labels)
+
+    return input_ids, labels
+
+if __name__ == '__main__':
+    sentences = ['I love you']
+    labels = [0]
+    print(tokenize(sentences, labels))
